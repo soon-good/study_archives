c2a023fabc4462179144bd48ae401b150bbb8be8
sgjung <playmymind@daum.net>
2020-04-13 11:21:43 +0900

    App Header 컴포넌트 추가

---
+++ frontend/vuejs/examples/memo-application/src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app-header">
+        <h1>메모 애플리케이션</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<style scoped>
+    .app-header {
+        overflow: hidden;
+        padding: 52px 0 27px;
+    }
+
+    .app-header h1{
+        float: left;
+        font-size: 24px;
+        text-align: center;
+    }
+</style>
