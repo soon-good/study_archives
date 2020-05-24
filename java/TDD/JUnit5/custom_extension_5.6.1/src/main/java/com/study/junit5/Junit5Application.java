0098654d7c45b20ec4c693efe20c96b29b8d32b3
sgjung <playmymind@daum.net>
2020-05-24 21:19:00 +0900

    JUnit5.6.1 기반 커스텀 어노테이션

---
+++ java/TDD/JUnit5/custom_extension_5.6.1/src/main/java/com/study/junit5/Junit5Application.java
@@ -0,0 +1,13 @@
+package com.study.junit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5Application.class, args);
+	}
+
+}
