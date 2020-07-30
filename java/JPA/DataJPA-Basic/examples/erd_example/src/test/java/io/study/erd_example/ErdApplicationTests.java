ea0247abdf01bd2c44d7a4ee2a09db1d8ab88aec
sgjung <playmymind@daum.net>
2020-07-31 00:01:00 +0900

    JPA - Emp 스키마 기본 예제

---
+++ java/JPA/DataJPA-Basic/examples/erd_example/src/test/java/io/study/erd_example/ErdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.erd_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
