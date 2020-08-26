14cf8b8fce8186a9bbefb7738d807cc7c2649946
sgjung <playmymind@daum.net>
2020-08-26 22:26:30 +0900

    Feign Client 사용예 (1)  : 프로젝트 초기 구성 커밋

---
+++ java/spring/rest-client/feign/examples/simple-feign-client/src/main/java/io/simple/simplefeign/FeignApplication.java
@@ -0,0 +1,15 @@
+package io.simple.simplefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignApplication.class, args);
+	}
+
+}
